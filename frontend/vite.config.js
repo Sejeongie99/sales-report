da100a48071bf61dcefe3d5f9648c051a5fb6fdd
Margie Dosdos <mmdosdos@yssp.ph>
2023-05-21 16:30:36 +0800

    trial

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
     // port: 3000,
     proxy: {
       '/api': {
-        // target: 'http://localhost:5000',
-        target: 'https://sales-report-um9k.onrender.com',
+        target: 'http://localhost:5000',
+        // target: 'https://sales-report-um9k.onrender.com',
         changeOrigin: true,
         secure: false,
       },
