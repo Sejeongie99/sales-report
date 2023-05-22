e609e33594402afecd308e2ad3821c4f25f2142f
Margie Dosdos <mmdosdos@yssp.ph>
2023-05-23 07:53:19 +0800

    trial

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 //mongoDB connection import
 import connectDB from './config/db.js'
 
-//check port
-const port = process.env.PORT || 5000
-
 //api routes import
 import userRoutes from './routes/userRoutes.js'
+
+//check port
+const port = process.env.PORT || 5000
 
 //mongoDB connection
 connectDB()
@@ -29,6 +29,9 @@ app.use(express.urlencoded({ extended: true }))
 
 //cookie parser
 app.use(cookieParser())
+
+//api routes uses
+app.use('/api/users', userRoutes)
 
 // if (process.env.NODE_ENV === 'production') {
 //   const __dirname = path.resolve()
@@ -42,9 +45,6 @@ app.use(cookieParser())
 // }
 
 app.get('/', (req, res) => res.send('Server is ready'))
-
-//api routes uses
-app.use('/api/users', userRoutes)
 
 //error middleware handling uses
 app.use(notFound)
